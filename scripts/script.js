2157a703d76fc9b59ee822c9332dd3b2b0023d97
Ben Mahowald <ben.mahowald@gmail.com>
2016-08-31 19:35:08 -0500

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+// console.log('js is sourced');
+
+var results='';
+
+$(document).ready(function () {
+  // console.log('in ready');
+
+  $('#searchMe').on('click', function () {
+    // console.log('I clicked the button');
+    var movieInput = $('#movieIn').val();
+    // console.log(movieInput);
+    var searchURL = 'http://www.omdbapi.com/?s=' + movieInput;
+    // console.log(searchURL);
+    $.ajax({
+      url: searchURL,
+      dataType: 'JSON',
+      success: function (data) {
+        // console.log(data.Search);
+
+        displayMovies(data);
+        clearField();
+
+      },//end success
+    });//end ajax
+  });//end button click
+});//end ready
+
+var displayMovies = function (data) {
+  // console.log('in displayMovies');
+  console.log(data.Search);
+  $('#results').empty();
+
+  for (var i = 0; i < data.Search.length; i++) {
+  results += '<h2>' + data.Search[i].Title + '</h1><p><img src="' + data.Search[i].Poster
+  + '"></p><p>' + data.Search[i].Year + '</p>';
+  }
+$('#results').html(results);
+};//end displayMovies
+
+var clearField = function () {
+  $('#movieIn').val('');
+};
